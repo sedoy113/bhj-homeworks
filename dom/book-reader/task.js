b4926ae834651c2e89b37e846458ea1de355a41c
sedoy113 <sedoy113@yandex.ru>
2021-05-10 20:10:54 +0300

    book

---
+++ dom/book-reader/task.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const book = document.querySelector('.book');
+const bookControl = document.querySelector('.book__control');
+
+function fontChange(el) {
+	el.preventDefault();
+
+	if (el.target.dataset.size === 'big') {
+		book.classList.add('book_fs-big');
+		book.classList.remove('book_fs-small');
+	} else if (el.target.dataset.size === 'small') {
+		book.classList.add('book_fs-small');
+		book.classList.remove('book_fs-big');
+	} else {
+		book.classList.remove('book_fs-big');
+		book.classList.remove('book_fs-small');
+	}
+
+	for (let letter of document.querySelectorAll('.font-size')) {
+		letter.classList.remove('font-size_active');
+	}
+
+	el.target.classList.add('font-size_active');
+
+}
+
+for (let b of bookControl.children) {
+	b.addEventListener('click', fontChange);
+}
